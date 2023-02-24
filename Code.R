149f4f09ba82fef468fc0a816dd59b8c7a212f57
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-24 15:41:16 +0800

    Create Code.R

---
+++ Code.R
@@ -0,0 +1,5 @@
+#Place "genomes.fasta" in C:\Users\...\AppData\Local\R\win-library\4.2\ggmsa\extdata
+
+library(ggmsa)
+protein_sequences <- system.file("extdata", "genomes.fasta", package = "ggmsa")
+ggmsa(protein_sequences, start = 221, end = 280, char_width = 0.5, seq_name = T) + geom_seqlogo() + geom_msaBar()

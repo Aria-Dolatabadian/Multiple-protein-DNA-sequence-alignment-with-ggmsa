f33fe5c898aa08fb3583d0bc2012385a802dfaec
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-03 10:35:30 +0800

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,4 +1,4 @@
-#Place "genomes.fasta" in C:\Users\...\AppData\Local\R\win-library\4.2\ggmsa\extdata
+#Place "genomes.fasta" or "seq.fa" in C:\Users\...\AppData\Local\R\win-library\4.2\ggmsa\extdata
 
 library(ggmsa)
 protein_sequences <- system.file("extdata", "genomes.fasta", package = "ggmsa")
